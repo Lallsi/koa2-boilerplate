788d276e07299d2228ba965b95657fc65e865b31
Aarnio Lauri <aarniol1@niksula.hut.fi>
2016-11-27 07:06:53 +0200

    App done.

--- app.js
+++ app.js
@@ -22,7 +22,13 @@ var todoSchema = mongoose.Schema({
     done: Boolean
 });
 
+var idSchema = mongoose.Schema({
+    id: Number
+});
+
+
 var Todo = mongoose.model("Todo", todoSchema);
+var Id = mongoose.model("Id", idSchema);
 
 router.get('/api/v1/get', function *(next) {
 	
@@ -37,7 +43,6 @@ router.get('/api/v1/get', function *(next) {
 		 done: elem.done
 		};
 	    todos.push(todo);
-	    console.log(todo); 
 	  });
   });
   var res = {todos: todos};
@@ -46,23 +51,29 @@ router.get('/api/v1/get', function *(next) {
 });
 
 router.post('/api/v1/add', function *(next) {
-  console.log(this.request.body);
-  
-  var newId = 0;
-  
-  yield Todo.find().sort({id: -1}).limit(1).exec(
-    function (err, todo) {
+	var taskId = 0;
+  yield Id.find().exec(
+    function (err, id) {
       if(err) console.log(err);
-	  if(todo[0]) newId = todo[0].id+1;
-    }
+	  if(!id[0]) {
+		var newId = new Id({id: 0});
+		newId.save(function(err,res){
+		  if(err)
+			console.log("Error adding new id to database");
+		});
+	  }
+	  else {
+		Id.update({id: id[0].id}, {id: id[0].id+1}).exec();
+		taskId = id[0].id+1;
+	  }
+	}
   );
+  
   var todo = {
-	  id: newId,
+	  id: taskId,
 	  title: this.request.body.title,
 	  done: false
   };
-
-  console.log(todo);
 
   this.body = todo;
   
@@ -71,23 +82,36 @@ router.post('/api/v1/add', function *(next) {
 	title: todo.title,
 	done: todo.done
   });
-  
+
   yield newTodo.save(function(err, res){
 	if(err)
-	  console.log("DB ERROR");
+	  console.log("Error adding entry to database");
 	else
-	  console.log("NEW TASK ADDED");
+	  console.log( "Added task "+ JSON.stringify(todo));
   });
 });
 
-router.delete('/api/v1/remove', function *(next) {
-  console.log(this.request.body);
-  this.body = this.request.body;
+router.delete('/api/v1/remove/:id', function *(next) {
+  var task = yield Todo.find({id: this.params.id}).exec();
+  yield Todo.remove({id: this.params.id}).exec();
+  this.body = { id: this.params.id, removed: true };
+  var print = {
+	  id: task[0].id,
+	  title: task[0].title,
+	  done: task[0].done
+  };
+  console.log("Deleted task " + JSON.stringify(print));
 });
 
 router.put('/api/v1/done', function *(next) {
-  console.log(this.request.body);
-  this.body = this.request.body;
+  yield Todo.update({id: this.request.body.id}, {done: this.request.body.done}).exec();
+  var updated = yield Todo.find({id: this.request.body.id}).exec();
+  this.body = {
+	  id: updated[0].id,
+	  title: updated[0].title,
+	  done: updated[0].done
+  };
+  console.log("Updated task " + JSON.stringify(this.body) );
 });
 
 app.use(router.routes())
